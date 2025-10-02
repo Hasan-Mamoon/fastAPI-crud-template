9afcf2df5a5739145d922d0d84a1445d8a31c486
Hasan-Mamoon <mianhassan085@gmail.com>
2025-10-02 11:47:50 +0500

    refactor:main->related-files

---
+++ backend/app/api/v1/studentApi.py
@@ -0,0 +1,48 @@
+from fastapi import APIRouter, FastAPI,Depends, HTTPException
+from sqlmodel import select
+from app.database.session import Session,get_session
+from app.schemas.student import StudentCreate,StudentRead,StudentUpdate
+from app.models.student import student
+
+router = APIRouter(prefix="/students", tags=["students"])
+
+
+@router.get("/students", response_model=list[StudentRead])
+def read_users(session:Session= Depends(get_session)):
+    students = session.exec(select(student)).all()
+    return students
+
+@router.post("/add-students", response_model=StudentRead)
+def create_student(newStudent: StudentCreate, session: Session = Depends(get_session)):
+    session.add(newStudent)
+    session.commit()
+    session.refresh(newStudent)
+    return newStudent
+
+@router.patch("/edit-student",response_model=StudentRead)
+def edit_student(studentData: StudentUpdate, session: Session = Depends(get_session)):
+    res = session.exec(select(student).where(student.id == studentData.id))
+    db_student = res.first()
+
+    if not db_student:
+        raise HTTPException(status_code=404, detail="Student not found")
+
+    update_data = studentData.model_dump(exclude_unset=True)
+
+    for key, value in update_data.items():
+        setattr(db_student, key, value)
+
+    session.add(db_student)
+    session.commit()
+    session.refresh(db_student)
+
+    return db_student
+
+@router.delete("/delete-student",response_model=StudentRead)
+def delete_student(student_id:int,session:Session=Depends(get_session)):
+  toDelete = session.get(student,student_id)
+  if not toDelete:
+        raise HTTPException(status_code=404, detail="Student not found")
+  session.delete(toDelete)
+  session.commit()
+  return toDelete
--- backend/app/database/session.py
+++ backend/app/database/session.py
@@ -1,8 +1,10 @@
-from sqlmodel import SQLModel,create_engine,Session
+from sqlmodel import create_engine, SQLModel, Session
 
+DATABASE_URL = "sqlite:///./app/database/student.db"
+engine = create_engine(DATABASE_URL, echo=True)
 
-engine = create_engine("sqlite:///student.db")
-SQLModel.metadata.create_all(engine)
+def init_db():
+    SQLModel.metadata.create_all(engine)
 
 def get_session():
     with Session(engine) as session:
